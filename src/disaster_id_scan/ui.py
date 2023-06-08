f7ce4d5b8540ea2f94d6123cbddb4c92711e30e8
anjomro <github@anjomro.de>
2023-06-08 15:45:24 +0200

    Second draft

--- src/disaster_id_scan/ui.py
+++ src/disaster_id_scan/ui.py
@@ -1,20 +1,48 @@
+from pathlib import Path
+
 import toga
+from toga.style.pack import COLUMN, LEFT, RIGHT, ROW, Pack, BOTTOM
 
 
-def button_handler(widget):
-    print("hello")
+class DisasterUI(toga.App):
+    directory: Path
 
+    def startup(self):
+        # Main window of the application with title and size
+        self.main_window = toga.Window(title="Disaster ID Scanner")
 
-def build(app):
-    box = toga.Box()
+        box = toga.Box(style=Pack(direction=ROW, padding_top=100, alignment=BOTTOM))
 
-    button = toga.Button("Hello world", on_press=button_handler)
-    button.style.padding = 50
-    button.style.flex = 1
-    box.add(button)
+        kangoroo_example_image = toga.Image("/home/anjomro/repos/disaster-id-scan/300x200.jpg")
+        image_view = toga.ImageView(kangoroo_example_image, id="preview", style=Pack(width=300, height=200))
 
-    return box
+        select_dialogue = toga.Button("Select directory", on_press=self.select_directory_ui)
+        start_video = toga.Button("Start Video")
+
+        control_box = toga.Box(children=[
+            start_video,
+            select_dialogue
+        ])
+        box.add(image_view)
+        box.add(control_box)
+
+        # Add the content on the main window
+        self.main_window.content = box
+
+        # Show the main window
+        self.main_window.show()
+        self.select_directory_ui()
+
+    def selected_path(self, caller, path: Path):
+        if path is None:
+            print("Please Select a Path")
+        else:
+            self.directory = path
+        print("fini")
+
+    def select_directory_ui(self, *args):
+        self.main_window.select_folder_dialog(title="Select Saving Location", on_result=self.selected_path)
 
 
 def start_gui():
-    toga.App("Disaster ID scan", "de.anjomro.disaster-id-scan", startup=build).main_loop()
+    DisasterUI("Disaster ID scan", "de.anjomro.disaster-id-scan").main_loop()
